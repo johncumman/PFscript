01f3c29bc6ef77ea0c8d0d659104d2d91aaf60a0
johncumman <96276267+johncumman@users.noreply.github.com>
2021-12-17 14:35:04 -0800

    Update as.lua

--- as.lua
+++ as.lua
@@ -3,6 +3,8 @@
     local main = Window:NewTab("Main")
     local Player = Window:NewTab("Player")
     local PlayerSection = Player:NewSection("Player")
+    local Ammo = Window:NewTab("Ammo")
+    local AmmoSection = Ammo:NewSection("Ammo")
     PlayerSection:NewToggle("Arm Chams", "Arm Chams", function(state)
     if state then
         getgenv().Color = Color3.fromRGB(141, 115, 245)
@@ -39,6 +41,38 @@ end)
         print("Toggle Off")
     end
 end)
+
+AmmoSection:NewToggle("Ammo Combiner", "Combines ammo", function(state)
+    if state then
+        --made by chandler bby
+
+local GunHandler
+for i,v in next, getgc(true) do
+	if type(v) == "table" then
+		if rawget(v, "setsprintdisable", "setequipped") then
+			GunHandler = v
+		end
+	end
+	if GunHandler then break end
+end
+
+if not GunHandler then return game.Players.LocalPlayer:Kick("Failed to find GunHandler") end
+
+game:GetService("RunService").RenderStepped:Connect(function()
+	local Gun = GunHandler.currentgun
+	if Gun and Gun.setequipped then
+		local UpValues = debug.getupvalues(Gun.setequipped)
+		local Magazine, CurrentAmmo = UpValues[11], UpValues[10]
+		if Magazine ~= 0 then
+			debug.setupvalue(Gun.setequipped, 10, (Magazine+CurrentAmmo))
+			debug.setupvalue(Gun.setequipped, 11, 0)
+		end
+	end
+end)
+    else
+        print("fag")
+        end
+
     
     local Weapon = Window:NewTab("Weapon")
     local WeaponSection = Weapon:NewSection("Weapon")
@@ -49,4 +83,5 @@ end)
         end
     local Player = Player:NewTab("Player")
 end
+end)
 end)
